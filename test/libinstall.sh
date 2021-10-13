3e6378b1655132aa33d9a18d37d8e0ffefa397ef
ricet <ricet@rose-hulman.edu>
2021-10-13 21:51:55 +0000

    added circuitpython lib install script

---
+++ test/libinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo pip3 install adafruit-blinka
+sudo pip3 install adafruit-circuitpython-scd30
+sudo pip3 install adafruit-circuitpython-pct2075
+sudo pip3 install adafruit-circuitpython-bme680
+
+# If displayio ever works
+sudo pip3 install adafruit-blinka-displayio
+sudo pip3 install adafruit-circuitpython-displayio-sh1107
