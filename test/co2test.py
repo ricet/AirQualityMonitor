0ab3fee0af272ee25939b84314d621c16d27fdd2
ricet <ricet@rose-hulman.edu>
2021-10-13 05:36:06 +0000

    co2 works screen doesnt

---
+++ test/co2test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import time
+import board
+import adafruit_scd30
+
+i2c = board.I2C()   # uses board.SCL and board.SDA
+scd = adafruit_scd30.SCD30(i2c)
+
+while True:
+    # since the measurement interval is long (2+ seconds) we check for new data before reading
+    # the values, to ensure current readings.
+    if scd.data_available:
+        print("Data Available!")
+        print("CO2:", scd.CO2, "PPM")
+        print("Temperature:", scd.temperature, "degrees C")
+        print("Humidity:", scd.relative_humidity, "%%rH")
+        print("")
+        print("Waiting for new data...")
+        print("")
+
+    time.sleep(0.5)
---
+++ test/oledtest.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import board
+import displayio
+import terminalio
+from adafruit_display_text import bitmap_label as label # from adafruit_display_text
+import adafruit_displayio_sh1107
+
+displayio.release_displays()
+#oled_reset = board.D9
+
+# Use for I2C
+i2c = board.I2C()
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+
+# SH1107 is vertically oriented 64x128
+WIDTH = 128
+HEIGHT = 64
+BORDER = 2
+
+display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT)
+
+# Make the display context
+splash = displayio.Group()
+display.show(splash)
+
+color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0xFFFFFF  # White
+
+bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+splash.append(bg_sprite)
+
+# Draw a smaller inner rectangle in black
+inner_bitmap = displayio.Bitmap(WIDTH - BORDER * 2, HEIGHT - BORDER * 2, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0x000000  # Black
+inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER)
+splash.append(inner_sprite)
