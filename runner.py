dc3a8d68e3dcf59474eb2a562af6acd231b55072
rhit-zacherw <zacherw@rose-hulman.edu>
2021-11-14 19:51:58 -0500

    Added Gas and CO2 sensing, threading

--- runner.py
+++ runner.py
@@ -9,12 +9,41 @@ import adafruit_bme680
 from adafruit_pm25.i2c import PM25_I2C
 import busio
 from adafruit_pm25.i2c import PM25_I2C
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.ADC as ADC
+import signal, sys
+import threading
 
 
 DB_PATH = "/etc/grafana/airQualityData.db"
 con = sqlite3.connect(DB_PATH)
 cur = con.cursor()
 
+BEEP = "P9_42"
+GPIO.setup(BEEP, GPIO.OUT)
+GPIO.output(BEEP, 0)
+
+GAS_HEAT_EN = "P9_23"
+GPIO.setup(GAS_HEAT_EN, GPIO.OUT)
+GPIO.output(GAS_HEAT_EN, 1)
+
+CO_HEAT_EN = "P9_41"
+GPIO.setup(CO_HEAT_EN, GPIO.OUT)
+GPIO.output(CO_HEAT_EN, 1)
+
+CO_HEAT_CTL = "P9_26"
+GPIO.setup(CO_HEAT_CTL, GPIO.OUT)
+GPIO.output(CO_HEAT_CTL, 1)
+
+ADC.setup()
+
+def signal_handler(sig, frame):
+    GPIO.output(GAS_HEAT_EN, 0)
+    GPIO.output(CO_HEAT_EN, 0)
+    GPIO.output(CO_HEAT_CTL, 0)
+    GPIO.output(BEEP, 0)
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 i2c = board.I2C()
 scd = adafruit_scd30.SCD30(i2c)
@@ -69,12 +98,24 @@ def getParticles():
     except RuntimeError:
         print("Unable ro read from sensor, retrying...")
 
+def gasThread():
+    global gas_value
+    global co_value
+    GPIO.output(CO_HEAT_CTL, 0)
+    time.sleep(30)
+    gas_value = ADC.read_raw("P9_37")
+    co_value = ADC.read_raw("P9_39")
+    print("gas: " + str(gas_value))
+    print("co: " + str(co_value))
+    time.sleep(1)
+    GPIO.output(CO_HEAT_CTL, 1)
+    time.sleep(60)
 
 try:
     # Create table
     cur.execute(
         """CREATE TABLE airQualityData
-               (date text, data_CO2 real, data_temp_bme real, data_gas_bme real, data_hum_bme real, data_pres_bme real, data_alt_bme real, env_pm10 real, env_pm25 real, env_pm100 real, particles_03um real, particles_05um real, particles_10um real, particles_25um real, particles_50um real, particles_100um real)"""
+               (date text, data_CO2 real, data_temp_bme real, data_gas_bme real, data_hum_bme real, data_pres_bme real, data_alt_bme real, env_pm10 real, env_pm25 real, env_pm100 real, particles_03um real, particles_05um real, particles_10um real, particles_25um real, particles_50um real, particles_100um real, gas_value real, co_value real)"""
     )
 
 # Insert a row of data
@@ -94,6 +135,12 @@ except:
     global particles_25um
     global particles_50um
     global particles_100um
+    global gas_value
+    global co_value
+    gas_value = -1
+    co_value = -1
+    gasThread = threading.Thread(target=gasThread)
+    gasThread.start()
     for tem in range(100):
         now = datetime.now()
         date_time = now.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -133,6 +180,10 @@ except:
             + str(particles_50um)
             + ","
             + str(particles_100um)
+            + ","
+            + str(gas_value)
+            + ","
+            + str(co_value)
             + ")"
         )
 
@@ -140,6 +191,8 @@ except:
         con.commit()
         time.sleep(5)
 
+    gasThread.stop()
+    gasThread.join()
     # We can also close the connection if we are done with it.
     # Just be sure any changes have been committed or they will be lost.
     con.close()
